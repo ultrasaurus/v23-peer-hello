45656daf49bb490425674d255e63a972b2c534cd
Sarah Allen <sarahallen@google.com>
2016-06-15 09:40:05 -0700

    diff user blessings

--- hello.go
+++ hello.go
@@ -4,20 +4,37 @@ import (
   "fmt"
   "os"
   vsecurity "v.io/x/ref/lib/security"
+  "github.com/manveru/faker"
 )
 
 
 func main() {
-  fmt.Printf("hello v23\n")
+  fake, err := faker.New("en")
+  if err != nil {
+    fmt.Errorf("Error setting up name: %v", err)
+    return
+  }
+  name := fake.FirstName()
+  fmt.Printf("hello %v\n", name)
+
   passphrase := ""   // get this from user eventually
 
-  if _, err := os.Stat("cred"); os.IsNotExist(err) {
-    p, err := vsecurity.CreatePersistentPrincipal("cred", []byte(passphrase))
+  fmt.Printf("will put stuff here %v\n", "cred/"+name)
+  dir := "cred/"+name
+  if _, err := os.Stat(dir); os.IsNotExist(err) {
+    p, err := vsecurity.CreatePersistentPrincipal(dir, []byte(passphrase))
     if err != nil {
-      fmt.Printf("Error creating principal: %v", err)
+      fmt.Errorf("Error creating principal: %v", err); return
+    }
+    blessings, err := p.BlessSelf(name)
+    if err != nil {
+      fmt.Errorf("BlessSelf(%q) failed: %v", name, err); return
+    }
+    if err := vsecurity.SetDefaultBlessings(p, blessings); err != nil {
+      fmt.Errorf("could not set blessings %v as default: %v", blessings, err)
       return
     }
-    fmt.Printf("Principal created: %v", p)
+    fmt.Printf("Bless you.\n\n")
   }
 
 }
