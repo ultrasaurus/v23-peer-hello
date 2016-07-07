b2bfc335047be1fbd12fcce2c65cafc982bb2d08
Sarah Allen <sarahallen@google.com>
2016-07-06 19:36:43 -0700

    wip sketch: advertise / scan

--- hello.go
+++ hello.go
@@ -2,6 +2,7 @@ package main
 
 import (
   "bufio"
+  "flag"
   "fmt"
   "os"
   vsecurity "v.io/x/ref/lib/security"
@@ -15,19 +16,28 @@ import (
 )
 
 func main() {
-  fake, err := faker.New("en")
-  if err != nil {
-    fmt.Errorf("Error setting up name: %v", err)
-    return
+  var name string
+  flag.StringVar(&name, "id", "", "identifies unique instance, usually a name")
+  flag.Parse()
+
+  if name == "" {
+    fake, err := faker.New("en")
+    if err != nil {
+      fmt.Errorf("Error setting up name: %v", err)
+      return
+    }
+    name = fake.FirstName()
   }
-  name := fake.FirstName()
+
   fmt.Printf("hello %v\n", name)
 
   passphrase := ""   // get this from user eventually
 
-  fmt.Printf("will put stuff here %v\n", "cred/"+name)
+  fmt.Printf("will put credentials here %v\n", "cred/"+name)
   dir := "cred/"+name
-  if _, err := os.Stat(dir); os.IsExist(err) {
+  _, tmp_err := os.Stat(dir)
+  fmt.Printf("os.Stat(dir) err=%v\n", tmp_err);
+  if _, err := os.Stat(dir); err == nil {
     fmt.Printf("should load pricipal and do something with it.\n\n")
 
     // p, err := vsecurity.LoadPersistentPrincipal(dir, []byte(passphrase))
@@ -53,7 +63,17 @@ func main() {
 
   ctx, shutdown := v23.Init()
   defer shutdown()
-  _, _, err = v23.WithNewServer(ctx, "", ifc.HelloServer(service.Make()), nil)
+  _, server, err := v23.WithNewServer(ctx, "", ifc.HelloServer(service.Make()), security.AllowEveryone())
+
+  // start server with random blessing
+  // ep: (@host:port@...@blessing)
+  // send an advertisement: with (localmomentID, endpoint)
+  AdvertiseServer(ctx, nil, server, "")
+
+  // receive an advertisement
+  Scan()  // print who is nearby
+  
+
   if err != nil {
     log.Panic("Error listening: ", err)
   }
@@ -66,6 +86,12 @@ func main() {
     text = scanner.Text()
     if text == "bye" { break }
     fmt.Printf("echo %v\n", scanner.Text())
+    // f := ifc.HelloClient(*server)
+    // ctx, cancel := context.WithTimeout(ctx, time.Minute)
+    // defer cancel()
+    // hello, _ := f.Get(ctx)
+    // fmt.Println(hello)
+
   }
 
   fmt.Printf("bye bye.\n\n")
